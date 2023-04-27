e234f47a3a0ad33505f3443a9d0fb6eb674fbfa6
codewithkevin <oseikelvin709@gmail.com>
2023-04-27 10:17:22 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import NavMenu from "./pages/NavMenu";
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div>
       <NavMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
