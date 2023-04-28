e6a53156749b0bb809aa69792670d969ef0ce936
codewithkevin <oseikelvin709@gmail.com>
2023-04-28 01:10:19 +0000

    Created Hero Section

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Carousel, Button } from "antd";
 import { BiPlayCircle } from "react-icons/bi";
+import slide1 from "../../assests/Hero/slide1.png";
+import slide2 from "../../assests/Hero/slide2.png";
+import slide3 from "../../assests/Hero/slide3.png";
+import slide4 from "../../assests/Hero/slide4.png";
 
 const Hero = () => {
   const slideOne = {
     height: "870px",
     color: "#fff",
     lineHeight: "160px",
-    backgroundImage: `url('https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80')`,
+    backgroundImage: `url(${slide1})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
@@ -16,7 +20,7 @@ const Hero = () => {
     height: "870px",
     color: "#fff",
     lineHeight: "160px",
-    backgroundImage: `url('https://images.unsplash.com/photo-1559825481-12a05cc00344?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8b2NlYW58ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60')`,
+    backgroundImage: `url(${slide2})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
@@ -25,7 +29,7 @@ const Hero = () => {
     height: "870px",
     color: "#fff",
     lineHeight: "160px",
-    backgroundImage: `url('https://images.unsplash.com/photo-1548032885-b5e38734688a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=956&q=80')`,
+    backgroundImage: `url(${slide3})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
@@ -34,7 +38,7 @@ const Hero = () => {
     height: "870px",
     color: "#fff",
     lineHeight: "160px",
-    backgroundImage: `url('https://images.unsplash.com/photo-1507166763745-bfe008fbb831?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')`,
+    backgroundImage: `url(${slide4})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
@@ -44,29 +48,27 @@ const Hero = () => {
       <Carousel autoplay>
         <div>
           <div style={slideOne}>
-            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
-
-            <div className="absolute top-1/4 left-4 lg:left-28  w-full flex flex-col">
-              <div className="w-[450px] space-y-7 mb-14">
+            <div className="relative top-1/4 left-4 lg:left-28  w-full flex flex-col">
+              <div className="w-[450px] space-y-7 mb-10">
                 <h1 className="text-white  max-w-2xl text-4xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-5xl font-bold tracking-wide z-10 relative">
                   Real time water quality monitoring and management in fish
                   farms.
                 </h1>
-                <p className="w-[350px]">
+                <p className="w-[350px] text-white">
                   Empowering Aquaculture Production Through Technology.
                 </p>
               </div>
 
               <div className="btnHolder space-x-5 flex flex-row top-10">
                 <Button
-                  className="bg-[#146A96] font-medium cursor-pointer"
+                  className="bg-[#146A96] font-medium"
                   type="primary"
                   size="large"
                 >
                   Visit AquaStall
                 </Button>
                 <Button
-                  className="bg-white text-[#146A96] font-medium w-[150px] cursor-pointer"
+                  className="bg-white text-[#146A96] font-medium w-[150px]"
                   size="large"
                   style={{ display: "flex", alignItems: "center" }}
                 >
@@ -79,31 +81,102 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div>
-          <div style={slideOne}>
-            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
 
-            <div className="absolute top-1/4 left-4 lg:left-28  w-full flex flex-col">
-              <div className="w-[450px] space-y-7 mb-14">
+        <div>
+          <div style={slideTwo}>
+            <div className="relative top-1/4 left-4 lg:left-28  w-full flex flex-col">
+              <div className="w-[450px] space-y-7 mb-10">
                 <h1 className="text-white  max-w-2xl text-4xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-5xl font-bold tracking-wide z-10 relative">
                   Real time water quality monitoring and management in fish
                   farms.
                 </h1>
-                <p className="w-[350px]">
+                <p className="w-[350px] text-white">
                   Empowering Aquaculture Production Through Technology.
                 </p>
               </div>
 
               <div className="btnHolder space-x-5 flex flex-row top-10">
                 <Button
-                  className="bg-[#146A96] font-medium cursor-pointer"
+                  className="bg-[#146A96] font-medium"
                   type="primary"
                   size="large"
                 >
                   Visit AquaStall
                 </Button>
                 <Button
-                  className="bg-white text-[#146A96] font-medium w-[150px] cursor-pointer"
+                  className="bg-white text-[#146A96] font-medium w-[150px]"
+                  size="large"
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <span className="mr-2">
+                    <BiPlayCircle />
+                  </span>
+                  <span className="text-[#146A96]">Demo</span>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div style={slideThree}>
+            <div className="relative top-1/4 left-4 lg:left-28  w-full flex flex-col">
+              <div className="w-[450px] space-y-7 mb-10">
+                <h1 className="text-white  max-w-2xl text-4xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-5xl font-bold tracking-wide z-10 relative">
+                  Real time water quality monitoring and management in fish
+                  farms.
+                </h1>
+                <p className="w-[350px] text-white">
+                  Empowering Aquaculture Production Through Technology.
+                </p>
+              </div>
+
+              <div className="btnHolder space-x-5 flex flex-row top-10">
+                <Button
+                  className="bg-[#146A96] font-medium"
+                  type="primary"
+                  size="large"
+                >
+                  Visit AquaStall
+                </Button>
+                <Button
+                  className="bg-white text-[#146A96] font-medium w-[150px]"
+                  size="large"
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <span className="mr-2">
+                    <BiPlayCircle />
+                  </span>
+                  <span className="text-[#146A96]">Demo</span>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div style={slideFour}>
+            <div className="relative top-1/4 left-4 lg:left-28  w-full flex flex-col">
+              <div className="w-[450px] space-y-7 mb-10">
+                <h1 className="text-white  max-w-2xl text-4xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-5xl font-bold tracking-wide z-10 relative">
+                  Real time water quality monitoring and management in fish
+                  farms.
+                </h1>
+                <p className="w-[350px] text-white">
+                  Empowering Aquaculture Production Through Technology.
+                </p>
+              </div>
+
+              <div className="btnHolder space-x-5 flex flex-row top-10">
+                <Button
+                  className="bg-[#146A96] font-medium"
+                  type="primary"
+                  size="large"
+                >
+                  Visit AquaStall
+                </Button>
+                <Button
+                  className="bg-white text-[#146A96] font-medium w-[150px]"
                   size="large"
                   style={{ display: "flex", alignItems: "center" }}
                 >
