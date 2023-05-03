6802ed4fa7c4255839d0919cb9c9168c3207f189
codewithkevin <oseikelvin709@gmail.com>
2023-05-03 13:09:41 +0000

    Update

---
+++ src/components/About/Team.js
@@ -0,0 +1,41 @@
+import person1 from "../../assests/About/frank.png";
+import person2 from "../../assests/About/lady.png";
+import person3 from "../../assests/About/gabi.png";
+
+const Team = () => {
+  return (
+    <section>
+      <div>
+        <div className="flex flex-col items-center text-center">
+          <h2 className="font-bold text-2xl my-4">Meet Our Team</h2>
+          <p>
+            Our philosophy is simple — hire a team of diverse, passionate people
+            and foster a culture that empowers you to do your best work.
+          </p>
+        </div>
+
+        <div className="grid md:flex sm:grid-cols lg:flex md:items-center justify-center lg:flex-row lg:justify-evenly lg:mx-4 p-5">
+          <div className="sm:col-span-1 lg:col-auto p-4 lg:mb-20 text-center space-y-3">
+            <img className="rounded-full" src={person1} alt="" />
+            <h1 className="font-bold font-serif">Frank Owusu</h1>
+            <h6 className="text-[#146A96]">Co-Founder & CEO</h6>
+          </div>
+          <div className="sm:col-span-1 lg:col-auto p-4 lg:mb-20 text-center space-y-3">
+            <img className="rounded-full" src={person2} alt="" />
+            <h1 className="font-bold font-serif">Priscilla Brempong</h1>
+            <h6 className="text-[#146A96]">Co-Founder & CMO</h6>
+          </div>
+          <div className="sm:col-span-1 lg:col-auto p-4 lg:mb-20 text-center space-y-3">
+            <img className="rounded-full" src={person3} alt="" />
+            <h1 className="font-bold font-serif">Gabriel Lorlornyo</h1>
+            <h6 className="text-[#146A96]">Co-Founder & CEO</h6>
+          </div>
+         
+         
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Intro from "../components/About/Intro";
 import About from "../components/Home/About";
+import Team from "../components/About/Team";
 
 const Aboutus = () => {
   return (
-    <div className="w-full h-full mt-20 mb-30 mb-96">
+    <div className="w-full h-full mt-20 mb-5">
       <Intro />
+      <Team />
     </div>
   );
 };
