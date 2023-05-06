5542a4d9b489ebf1de9945bfb9d51fb301762494
codewithkevin <oseikelvin709@gmail.com>
2023-05-06 08:01:59 +0000

    created pop screen

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -4,6 +4,27 @@ import slide1 from "../../assests/Hero/slide1.png";
 import slide2 from "../../assests/Hero/slide2.png";
 import slide3 from "../../assests/Hero/slide3.png";
 import slide4 from "../../assests/Hero/slide4.png";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import * as React from "react";
+import logo from "../../assests/logo2.png";
+import CloseIcon from "@mui/icons-material/Close";
+import TextField from "@mui/material/TextField";
+import PhoneInput from "react-phone-number-input";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>,
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Hero = () => {
   const slideOne = {
@@ -42,6 +63,18 @@ const Hero = () => {
     backgroundPosition: "center",
   };
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [value, setValue] = React.useState();
+
   return (
     <div loading="lazy" className="relative h-[100vh] w-full justify-center">
       <Carousel autoplay>
@@ -67,6 +100,7 @@ const Hero = () => {
                   Visit AquaStall
                 </Button>
                 <Button
+                  onClick={handleClickOpen}
                   className="bg-white text-[#146A96] font-medium w-[150px]"
                   size="large"
                   style={{ display: "flex", alignItems: "center" }}
@@ -76,6 +110,94 @@ const Hero = () => {
                   </span>
                   <span className="text-[#146A96]">Demo</span>
                 </Button>
+
+                <Dialog
+                  open={open}
+                  TransitionComponent={Transition}
+                  keepMounted
+                  onClose={handleClose}
+                  aria-describedby="alert-dialog-slide-description"
+                >
+                  <div className="p-5 flex flex-col w-full space-y-5">
+                    <div className="flex flex-row justify-between">
+                      <div className="float-left">
+                        <img src={logo} alt="" />
+                      </div>
+
+                      <div className="float-right mt-5 cursor-pointer">
+                        <CloseIcon style={{ fontSize: "2rem" }} />
+                      </div>
+                    </div>
+
+                    <div className="form-container space-y-2 mx-auto max-w-lg">
+                      <h2 className="font-serif font-bold leading-[28px] text-[18px]">
+                        Request a Demo
+                      </h2>
+                      <p>
+                        Provide your details below to get access to the demo of
+                        Aquamet Solution
+                      </p>
+                    </div>
+
+                    <div className="flex md:flex-row sm:flex-col md:space-y-0 sm:space-y-10 md:justify-between gap-0 md:gap-[5rem] lg:gap-[5rem]">
+                      <div className="flex flex-col">
+                        <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
+                          First name
+                        </h6>
+                        <TextField
+                          id="outlined-basic"
+                          label="First Name"
+                          variant="outlined"
+                        />
+                      </div>
+                      <div className="flex flex-col">
+                        <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
+                          Last name
+                        </h6>
+                        <TextField
+                          id="outlined-basic"
+                          label="Last Name"
+                          variant="outlined"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
+                        Email
+                      </h6>
+                      <TextField
+                        id="outlined-basic"
+                        label="example@gmail.com"
+                        variant="outlined"
+                      />
+                    </div>
+
+                    <div className="flex flex-col">
+                      <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
+                        Phone Number
+                      </h6>
+                      <div className="flex items-center border border-gray-300 rounded-md p-2">
+                        <PhoneInput
+                          placeholder="Enter phone number"
+                          value={value}
+                          onChange={setValue}
+                          className="flex-1 outline-none"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex flex-row justify-between">
+                      <div className="float-left">
+                        <img src={logo} alt="" />
+                      </div>
+
+                      <div className="float-right mt-5 cursor-pointer">
+                        <CloseIcon style={{ fontSize: "2rem" }} />
+                      </div>
+                    </div>
+                  </div>
+                </Dialog>
               </div>
             </div>
           </div>
