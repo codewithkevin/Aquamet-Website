c5ed81164f2d9785d5de802f7d1b36f81d934576
Kelvin Osei <oseikelvin709@gmail.com>
2023-05-22 10:57:04 +0000

    Working on New Farmers Request

--- src/components/Essentials/PopUp/NewFarmers.js
+++ src/components/Essentials/PopUp/NewFarmers.js
@@ -1,12 +1,17 @@
 import * as React from "react";
-import logo from "../../assests/logo2.png";
+import logo from "../../../assests/logo2.png";
 import CloseIcon from "@mui/icons-material/Close";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
 import Button from "@mui/material/Button";
-import PhonePicker from '../PhonePicker';
+import PhonePicker from "../PhonePicker";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -27,7 +32,7 @@ const NewFarmers = ({ open, handleClose }) => {
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <div className="p-5 flex flex-col w-full space-y-5">
+        <div className="p-5 flex flex-col w-full space-y-10">
           <div className="flex flex-row justify-between">
             <div className="float-left">
               <img src={logo} alt="" />
@@ -43,32 +48,22 @@ const NewFarmers = ({ open, handleClose }) => {
 
           <div className="form-container space-y-2 mx-auto max-w-lg">
             <h2 className="font-serif font-bold leading-[28px] text-[18px]">
-              Request a Demo
+              Start Your Dream Fish Farm
             </h2>
             <p>
-              Provide your details below to get access to the demo of Aquamet
-              Solution
+              This form is intended to collect details in real time from
+              potential farmers who wants to start their farm. Read more
             </p>
           </div>
 
-          <div className="flex md:flex-row sm:flex-col md:space-y-0 sm:space-y-10 md:justify-between gap-0 md:gap-[5rem] lg:gap-[5rem]">
+          <div className="flex flex-col md:space-y-0 sm:space-y-10 md:justify-between gap-0 md:gap-[5rem] lg:gap-[5rem]">
             <div className="flex flex-col">
               <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
-                First name
+                Full name
               </h6>
               <TextField
                 id="outlined-basic"
-                label="First Name"
-                variant="outlined"
-              />
-            </div>
-            <div className="flex flex-col">
-              <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
-                Last name
-              </h6>
-              <TextField
-                id="outlined-basic"
-                label="Last Name"
+                label="Enter your name"
                 variant="outlined"
               />
             </div>
@@ -87,6 +82,133 @@ const NewFarmers = ({ open, handleClose }) => {
 
           <div>
             <PhonePicker />
+          </div>
+
+          <div className="flex md:flex-row sm:flex-col md:space-y-0 sm:space-y-10 md:justify-between gap-0 md:gap-[5rem] lg:gap-[5rem]">
+            <div className="flex flex-col">
+              <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
+                Age
+              </h6>
+              <TextField
+                id="outlined-basic"
+                label="Eg: 25"
+                variant="outlined"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col my-5 space-y-1">
+            <h6 className="font-serif text-[14px] leading-[20px]  ml-1 font-bold">
+              ID Type<span className="text-red-500">*</span>
+            </h6>
+            <FormControl>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+              >
+                <FormControlLabel
+                  value="Ghana card"
+                  control={<Radio />}
+                  label="Ghana card"
+                />
+                <FormControlLabel
+                  value="Passport"
+                  control={<Radio />}
+                  label="Passport"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+
+          <div className="flex flex-col">
+            <h6 className="font-serif text-[14px] leading-[20px]  ml-1 font-bold">
+              ID Number<span className="text-red-500">*</span>
+            </h6>
+            <TextField
+              id="outlined-basic"
+              label="Enter ID Number"
+              variant="outlined"
+            />
+          </div>
+
+          <div className="flex flex-col my-5 space-y-1">
+            <h6 className="font-serif text-[14px] leading-[20px]  ml-1 font-bold">
+              Current Location*<span className="text-red-500">*</span>
+            </h6>
+            <FormControl>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+              >
+                <FormControlLabel
+                  value="Ghana"
+                  control={<Radio />}
+                  label="Ghana"
+                />
+                <FormControlLabel value="USA" control={<Radio />} label="USA" />
+                <FormControlLabel
+                  value="Europe"
+                  control={<Radio />}
+                  label="Europe"
+                />
+                <div className="flex flex-row ">
+                  <FormControlLabel
+                    value="Others"
+                    control={<Radio />}
+                    label="Others"
+                  />
+                  <TextField id="standard-basic" variant="standard" />
+                </div>
+              </RadioGroup>
+            </FormControl>
+          </div>
+
+          <div className="flex flex-col">
+            <h6 className="font-serif text-[14px] leading-[20px] mb-2 ml-1">
+              Where in Ghana will your fish farm be situated? and the GPS
+              address of the intended site(if any)*
+            </h6>
+            <TextField
+              id="outlined-basic"
+              label="Enter your answer"
+              variant="outlined"
+            />
+          </div>
+
+          <div className="flex flex-col my-5 space-y-1">
+            <h6 className="font-serif text-[14px] leading-[20px]  ml-1 font-bold">
+              Which of the following facilities do you intend to use for your
+              fish farm.
+            </h6>
+            <FormControl>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+              >
+                <FormControlLabel
+                  value="Ghana"
+                  control={<Radio />}
+                  label="Ghana"
+                />
+                <FormControlLabel value="USA" control={<Radio />} label="USA" />
+                <FormControlLabel
+                  value="Europe"
+                  control={<Radio />}
+                  label="Europe"
+                />
+                <div className="flex flex-row ">
+                  <FormControlLabel
+                    value="Others"
+                    control={<Radio />}
+                    label="Others"
+                  />
+                  <TextField id="standard-basic" variant="standard" />
+                </div>
+              </RadioGroup>
+            </FormControl>
           </div>
 
           <div className="mx-5">
--- src/components/Products/Start.js
+++ src/components/Products/Start.js
@@ -1,21 +1,45 @@
-import React from "react";
+import { useState } from "react";
+import NewFarmers from "../Essentials/PopUp/NewFarmers";
 
 const Start = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <section class="flex flex-col justify-center items-center space-y-10 pb-20 bg-[#ECFBFF]">
-      <div class="max-w-4xl mx-auto">
-        <h1 class="text-4xl font-bold text-center mb-6 mt-6">
+    <section className="flex flex-col justify-center items-center space-y-10 pb-20 bg-[#ECFBFF]">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold text-center mb-6 mt-6">
           Start your small-scale to large-scale farm
         </h1>
-        <div class="text-center max-w-3xl">
+        <div className="text-center max-w-3xl">
           <p className="text-xl font-medium">
             We assist you source your and construct your ponds or tanks, provide
             you with access to quality feed, fingerlings and the right tools to
             maximize your yield
           </p>
-          <button class="bg-white  text-[#146A96] mt-10 font-normal py-2 px-4 rounded border-[#146A96]">
+
+          <button
+            onClick={handleClickOpen}
+            className="bg-white  text-[#146A96] mt-10 font-normal py-2 px-4 rounded border-[#146A96]"
+          >
             Get Started Now
           </button>
+        </div>
+      </div>
+
+      <div className="relative">
+        {open && (
+          <div className="fixed inset-0 bg-opacity-50 backdrop-filter backdrop-blur-lg"></div>
+        )}
+        <div className="relative z-10">
+          <NewFarmers open={open} handleClose={handleClose} />
         </div>
       </div>
     </section>
