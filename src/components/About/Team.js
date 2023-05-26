65b963c29942bfdfa1dd19a55fc5483e466ba99e
Kelvin Osei <oseikelvin709@gmail.com>
2023-05-26 07:38:43 +0000

    Update

--- src/components/About/Team.js
+++ src/components/About/Team.js
@@ -5,7 +5,6 @@ import member1 from "../../assests/About/member1.jpeg";
 import member2 from "../../assests/About/member2.jpeg";
 import member3 from "../../assests/About/member3.jpeg";
 
-
 const Team = () => {
   return (
     <section>
@@ -38,14 +37,14 @@ const Team = () => {
           <h2 class="font-bold text-2xl my-4">Board Members</h2>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 justify-center mx-4 p-5">
-          <div class="col-span-1 p-4 mb-20 text-center space-y-3">
+        <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 sm:gap-1 gap-4  justify-center mx-4 p-5">
+          <div class="col-span-1 p-4 mb-10 text-center space-y-3">
             <img class="rounded-full w-32 h-32 mx-auto" src={member1} alt="" />
             <h1 class="font-bold font-serif">Prof. Irene S. Egyir</h1>
             <h6 class="text-[#146A96]">Dean, School of Agriculture UG.</h6>
           </div>
 
-          <div class="col-span-1 p-4 mb-20 text-center space-y-3">
+          <div class="col-span-1 p-4 mb-10 text-center space-y-3">
             <img class="rounded-full w-32 h-32 mx-auto" src={member2} alt="" />
             <h1 class="font-bold font-serif">Dr. Godfred Owusu Boateng.</h1>
             <h6 class="text-[#146A96]">
@@ -53,7 +52,7 @@ const Team = () => {
             </h6>
           </div>
 
-          <div class="col-span-1 p-4 mb-20 text-center space-y-3">
+          <div class="col-span-1 p-4 mb-10 text-center space-y-3">
             <img class="rounded-full w-32 h-32 mx-auto" src={member3} alt="" />
             <h1 class="font-bold font-serif">Michael K. Adonteng</h1>
             <h6 class="text-[#146A96]">CEO, Ovules Consulting, UK</h6>
