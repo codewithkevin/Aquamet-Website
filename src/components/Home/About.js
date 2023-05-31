f593a19568f43f6ef6d1f4f691ac9ecf58802197
Kelvin Osei <oseikelvin709@gmail.com>
2023-05-31 21:17:37 +0000

    Adding Animation

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,11 +1,5 @@
 import ug from "../../assests/Others/Person1.png";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
-import {
-  useScrollFadeAnimation,
-  useScrollFadeAnimationLeft,
-  useScrollFadeAnimationRight,
-} from "../Essentials/useScrollFadeAnimation";
 
 const About = () => {
   const navigate = useNavigate();
@@ -20,64 +14,54 @@ const About = () => {
     navigate("/contact");
   }
 
-  const controls = useScrollFadeAnimation();
-  const controlsLeft = useScrollFadeAnimationLeft();
-  const controlsRight = useScrollFadeAnimationRight();
-
   return (
     <section className="mt-5 md:mt-20  mx-5 mb-10">
       <div className="lg:flex lg:flex-row lg:justify-evenly grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2">
-        <motion.div
-          initial={{ opacity: 0, x: -300 }}
-          animate={controlsLeft}
-          transition={{ duration: 0.2 }}
-        >
-          <div className="md:mt-10 sm:mt-4">
-            <div className="md:max-w-3xl sm:max-w-[1rem] mb-4">
-              <strong className="text-[36px]  font-semibold leading-[38px] text-center font-dmsans">
-                Aquamet Technologies
-              </strong>
-            </div>
-            <div className="flow-root ... max-w-2xl  mt-2 md:space-y-2 space-y-5">
-              <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
-                Aquamet is a registered company in Ghana that seeks to eradicate
-                the numerous uncertainties in aquaculture production using
-                technology.
-              </p>
-              <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
-                Our solution comes with a smart probe for monitoring water
-                quality remotely via SMS, mobile app and advisory both
-                online(voice messages and interactive app) and offline.
-              </p>
-              <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
-                Our primary goal is to foster aquaculture production through
-                reducing high mortalities and increasing farmers’ yield.
-              </p>
-              <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
-                An array of assistive farm management tools such as feed
-                estimator, record keeping, and management tips can also be found
-                on our mobile app.
-              </p>
-            </div>
-
-            <div className="mt-5 flex md:flex-row sm:flex-col  justify-start md:space-x-5 sm:space-y-5 md:space-y-0">
-              <button
-                onClick={contactSubmit}
-                className="bg-[#ffffff] hover:bg-blue-900 hover:text-white text-black  top-0 rounded-lg  px-5 py-2 border-2"
-                type="button"
-              >
-                Get in touch
-              </button>
-              <button
-                onClick={handleSubmit}
-                className="bg-[#146A96] hover:bg-blue-900 text-white  top-0 rounded-lg  px-5 py-2"
-                type="button"
-              >
-                Our Solution
-              </button>
-            </div>
+        <div className="md:mt-10 sm:mt-4">
+          <div className="md:max-w-3xl sm:max-w-[1rem] mb-4">
+            <strong className="text-[36px]  font-semibold leading-[38px] text-center font-dmsans">
+              Aquamet Technologies
+            </strong>
           </div>
-        </motion.div>
+          <div className="flow-root ... max-w-2xl  mt-2 md:space-y-2 space-y-5">
+            <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
+              Aquamet is a registered company in Ghana that seeks to eradicate
+              the numerous uncertainties in aquaculture production using
+              technology.
+            </p>
+            <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
+              Our solution comes with a smart probe for monitoring water quality
+              remotely via SMS, mobile app and advisory both online(voice
+              messages and interactive app) and offline.
+            </p>
+            <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
+              Our primary goal is to foster aquaculture production through
+              reducing high mortalities and increasing farmers’ yield.
+            </p>
+            <p className="font-serif text-[18px] leading-[28px] text-gray-600 font-normal">
+              An array of assistive farm management tools such as feed
+              estimator, record keeping, and management tips can also be found
+              on our mobile app.
+            </p>
+          </div>
+
+          <div className="mt-5 flex md:flex-row sm:flex-col  justify-start md:space-x-5 sm:space-y-5 md:space-y-0">
+            <button
+              onClick={contactSubmit}
+              className="bg-[#ffffff] hover:bg-blue-900 hover:text-white text-black  top-0 rounded-lg  px-5 py-2 border-2"
+              type="button"
+            >
+              Get in touch
+            </button>
+            <button
+              onClick={handleSubmit}
+              className="bg-[#146A96] hover:bg-blue-900 text-white  top-0 rounded-lg  px-5 py-2"
+              type="button"
+            >
+              Our Solution
+            </button>
+          </div>
+        </div>
 
         <div loading="lazy" className="mb-10 mt-0 sm:mt-5 lg:mt-0 md:mt-[8rem]">
           <img
--- src/components/Home/Partners.js
+++ src/components/Home/Partners.js
@@ -2,17 +2,21 @@ import React from "react";
 import ug from "../../assests/Partners/ug.png";
 import absa from "../../assests/Partners/absa.png";
 import partner from "../../assests/Partners/partner.png";
+import AOS from "../Essentials/AOSInitializer";
 
 const Partners = () => {
   return (
     <div className="bg-[#B2E2FB]">
-      <div className="flex justify-center">
+      <div data-aos="fade-down" className="flex justify-center">
         <h2 className="font-bold leading-[24px] text-center text-[#475467] text-[20px] my-[50px] font-serif">
           Our Trusted Partners
         </h2>
       </div>
 
-      <div className="grid md:flex sm:grid-cols-2 lg:flex md:items-center md:justify-center lg:flex-row lg:justify-evenly lg:mx-4 p-5">
+      <div
+        data-aos="fade-left"
+        className="grid md:flex sm:grid-cols-2 lg:flex md:items-center md:justify-center lg:flex-row lg:justify-evenly lg:mx-4 p-5"
+      >
         <div className="sm:col-span-1 lg:col-auto p-4 lg:mb-20">
           <h1 className="font-extrabold text-xl">
             KOSMOS <br /> INNOVATION <br /> CENTER
