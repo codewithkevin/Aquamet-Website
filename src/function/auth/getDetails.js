9dbc3a00c7a9f01b20a481d9fa6f90281003da9a
Kelvin Osei <oseikelvin709@gmail.com>
2023-07-05 08:54:56 +0000

    update

--- src/function/auth/getDetails.js
+++ src/function/auth/getDetails.js
@@ -2,47 +2,39 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const GetUserDetails = () => {
-  const [token, setToken] = useState("");
-  const [userData, setUserData] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const parsedUser = JSON.parse(storedUser);
-    const userToken = parsedUser ? parsedUser.token : null;
-    setToken(userToken);
+    const fetchUserData = async () => {
+      try {
+        const storedUser = localStorage.getItem("user");
+        const parsedUser = storedUser ? JSON.parse(storedUser) : null;
+        const userToken = parsedUser?.token || "";
+
+        const response = await axios.get(
+          "https://aquamet.onrender.com/api/user/account/me",
+          {
+            headers: {
+              Authorization: `Bearer ${userToken}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          const { name } = response.data;
+          setUserData(name);
+          setError(null);
+        } else {
+          throw new Error("An error occurred while fetching user data.");
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchUserData();
   }, []);
 
-  useEffect(() => {
-    if (token) {
-      fetchUserData(token);
-    }
-  }, [token]);
-
-  const fetchUserData = async (token) => {
-    try {
-      const response = await axios.get(
-        "https://aquamet.onrender.com/api/user/account/me",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        const userData = response.data;
-        const { name } = userData; // Extract the name property from the userData object
-        setUserData(name);
-      } else {
-        console.error(
-          "An error occurred while fetching user data.",
-          response.status
-        );
-      }
-    } catch (error) {
-      console.error("An error occurred while fetching user data.", error);
-    }
-  };
-
-  return { userData, token };
+  return { userData, error };
 };
